49a847e118952186fb37dc8c3dca83556b64c0b0
RenanCampista <renan.campista@edu.ufes.br>
2025-11-28 21:26:43 -0300

    feat: create Transaction model with migrations

--- iupi/transactions/models.py
+++ iupi/transactions/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Transaction(models.Model):
+    TYPE_CHOICES = [
+        ("income", "Income"),
+        ("expense", "Expense"),
+    ]
+    
+    description = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=7, choices=TYPE_CHOICES)
+    date = models.DateField()
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="transactions",
+        null=True, # Deixar null até que a autenticação esteja implementada
+        blank=True
+    )
+    
+    def __str__(self):
+        return f"{self.description} - {self.amount} ({self.type})"

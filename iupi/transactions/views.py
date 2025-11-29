8a35ac626abb4146546fb324babbc86664a25cdc
RenanCampista <renan.campista@edu.ufes.br>
2025-11-29 00:05:39 -0300

    feat: add summary action to TransactionViewSet for transaction totals

--- iupi/transactions/views.py
+++ iupi/transactions/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Sum
+
 from .models import Transaction
 from .serializers import TransactionSerializer
 
@@ -28,4 +31,24 @@ class TransactionViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(type=type)
             
         return queryset
+    
+    @action(detail=False, methods=['get'])
+    def summary(self, request) -> Response:
+        """Retorna um resumo das transações, incluindo totais de receitas, despesas e saldo.
+
+        Args:
+            request (_type_): Requisição HTTP.
+        Returns:
+            Response: Resposta HTTP com o resumo das transações.
+        """
+        total_income = Transaction.objects.filter(type="income").aggregate(total=Sum('amount'))['total'] or 0
+        total_expense = Transaction.objects.filter(type="expense").aggregate(total=Sum('amount'))['total'] or 0
+        
+        summary_data = {
+            "total_income": f"{total_income:.2f}",
+            "total_expense": f"{total_expense:.2f}",
+            "net_balance": f"{total_income - total_expense:.2f}"
+        }
+        
+        return Response(summary_data)
     

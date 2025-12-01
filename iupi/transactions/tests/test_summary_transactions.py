dcb6598c0d2579e9c320303e30a50811bedc9a5a
RenanCampista <renan.campista@edu.ufes.br>
2025-11-30 22:49:04 -0300

    feat: add unit tests for transaction summary endpoint

---
+++ iupi/transactions/tests/test_summary_transactions.py
@@ -0,0 +1,36 @@
+from rest_framework import status
+
+from .base import BaseAPITestCase
+from ..models import Transaction
+
+
+class TransactionSummaryTest(BaseAPITestCase):
+    """"Testes para resumo de transações."""
+    def setUp(self):
+        super().setUp()
+        self.authenticate()
+        
+        Transaction.objects.create(
+            description="Salario",
+            amount=2000,
+            type="income",
+            date="2025-12-01",
+            user=self.user
+        )
+
+        Transaction.objects.create(
+            description="Netflix",
+            amount=50,
+            type="expense",
+            date="2025-12-01",
+            user=self.user
+        )
+        
+    def test_summary_transactions(self):
+        """Teste para obter o resumo das transações"""
+        response = self.client.get("/api/transactions/summary/") # TODO alterar a rota para /summary/
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['total_income'], '2000.00')
+        self.assertEqual(response.data['total_expense'], '50.00')
+        self.assertEqual(response.data['net_balance'], '1950.00')

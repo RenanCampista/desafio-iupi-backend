b13d62de0e75a269abdc6673b4ece2a81a4d068c
RenanCampista <renan.campista@edu.ufes.br>
2025-11-30 21:16:19 -0300

    feat: add pagination settings to REST framework configuration

--- iupi/iupi/settings.py
+++ iupi/iupi/settings.py
@@ -59,7 +59,9 @@ REST_FRAMEWORK = {
     ], # Only use JSON renderer
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
 
 ROOT_URLCONF = 'iupi.urls'

2328b4056156fdd848e7064c42e854faf70dc269
RenanCampista <renan.campista@edu.ufes.br>
2025-11-30 23:09:29 -0300

    feat: add unit tests for updating transactions

---
+++ iupi/transactions/tests/test_update_transactions.py
@@ -0,0 +1,52 @@
+from rest_framework import status
+
+from .base import BaseAPITestCase
+from ..models import Transaction
+
+
+class TransactionUpdateTest(BaseAPITestCase):
+    """Testes para atualização de transações."""
+    
+    def setUp(self):
+        super().setUp()
+        self.authenticate()
+        
+        self.transaction =Transaction.objects.create(
+            description="Salario",
+            amount=2000,
+            type="income",
+            date="2025-12-01",
+            user=self.user
+        )
+        
+    def test_patch_update_transaction(self):
+        """Teste para atualizar uma transação via PATCH."""
+        
+        payload = {
+            "amount": 2500,
+            "date": "2026-01-01"
+        }
+        
+        response = self.client.patch(f'/api/transactions/{self.transaction.id}/', payload)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['amount'], "2500.00")
+        self.assertEqual(response.data['date'], "2026-01-01")
+
+        
+    def test_put_update_transaction(self):
+        """Teste para atualizar uma transação via PUT."""
+        
+        payload = {
+            "description": "Salario atualizado",
+            "amount": 3000,
+            "type": "income",
+            "date": "2026-01-01"
+        }
+        
+        response = self.client.put(f'/api/transactions/{self.transaction.id}/', payload)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['description'], "Salario atualizado")
+        self.assertEqual(response.data['amount'], "3000.00")
+        self.assertEqual(response.data['date'], "2026-01-01")

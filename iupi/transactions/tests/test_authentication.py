27b634ee8c3abeba01cb1d9b6ad4149103fb14a4
RenanCampista <renan.campista@edu.ufes.br>
2025-11-30 23:24:18 -0300

    feat: add unit tests for authentication routes

---
+++ iupi/transactions/tests/test_authentication.py
@@ -0,0 +1,14 @@
+from rest_framework import status
+
+from .base import BaseAPITestCase
+
+
+class AuthenticationTest(BaseAPITestCase):
+    """Testes para autenticação das rotas protegidas"""
+
+    def test_cannot_access_transactions_without_token(self):
+        """Sem token deve retornar 401"""
+
+        response = self.client.get("/api/transactions/")
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
